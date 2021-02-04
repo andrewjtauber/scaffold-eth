52baa2e1e86e31da04c92fe3ab6c09af12a28550
andrewjtauber <andrewjohntauber@gmail.com>
2021-02-04 16:54:37 +1000

    just messing around

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -10,14 +10,57 @@ contract YourContract {
 
   string public purpose = "🛠 Programming Unstoppable Money";
 
+  address public owner;
+
+  address[] private stakers;
+  mapping(address => uint256) private balances;
+  uint256 public constant threshold = 1 ether;
+  mapping(address => bool) private hasStaked;
+
+  string public publicString = "PublicString";
+  string private privateString = "privateString";
+
+  uint256 public stakedAmmount;
+
+
   constructor() public {
     // what should we do on deploy?
+    owner = msg.sender;
   }
 
   function setPurpose(string memory newPurpose) public {
+    //require(msg.sender == owner, "ERROR: not owner");
     purpose = newPurpose;
     console.log(msg.sender,"set purpose to",purpose);
     emit SetPurpose(msg.sender, purpose);
+  }
+
+  // Stake
+  function Stake(uint256 _amount) public {
+    // Require amount is more than 0
+    require(_amount > 0, "ammount cannot be 0 or less");
+
+    // Transfer ETH to this contact from the staker
+    //("ETH_placholder").transferfrom(msg.sender, address(this), _amount);
+    // TODO: Check they have the balance
+    //ether.transferfrom(msg.sender, address(this), _amount);
+
+    // Update Amount Staked
+    balances[msg.sender] = balances[msg.sender] + _amount;
+    //stakedAmmount = balances[msg.sender];
+    // Add user to stakers array *only* if they haven't staked already
+    if(!hasStaked[msg.sender]) {
+      stakers.push(msg.sender);
+    }
+  }
+
+  // Find the amount an address has staked'
+  function getAmountStaked(address _address) public returns (uint256)  {
+    if (hasStaked[_address] != false) {
+        stakedAmmount = balances[_address];
+    } else {
+      stakedAmmount = 0;
+    }
   }
 
 }
--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -2,7 +2,10 @@ const { ethers } = require("hardhat");
 const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
 
+
+
 use(solidity);
+
 
 describe("My Dapp", function () {
   let myContract;
@@ -22,5 +25,41 @@ describe("My Dapp", function () {
         expect(await myContract.purpose()).to.equal(newPurpose);
       });
     });
+
+    describe("Stake()", function () {
+      it("Should be able to stake", async function () {
+        const newPurpose = "Test Purpose";
+
+        await myContract.setPurpose(newPurpose);
+        expect(await myContract.purpose()).to.equal(newPurpose);
+      });
+    });
   });
 });
+
+/*
+contract('YourContract', ([owner, investor]) => {
+  //let daiToken, dappToken, tokenFarm
+
+  before(async () => {
+    // Load Contracts
+
+    daiToken = await DaiToken.new()
+    dappToken = await DappToken.new()
+    YourContract = await YourContract.new()
+
+    // Transfer all Dapp tokens to farm (1 million)
+    await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+
+    // Send tokens to investor
+    await daiToken.transfer(investor, tokens('100'), { from: owner })
+  })
+
+  describe('Mock DAI deployment', async () => {
+    it('has a name', async () => {
+      const name = await daiToken.name()
+      assert.equal(name, 'Mock DAI Token')
+    })
+  })
+})
+*/
